5b541c4bf454ffc5a97754e4f7dedd8207f9f41d
Abdiqani Afrah <abdi@Abdiqanis-MacBook-Air.local>
2022-08-25 11:52:57 +0100

    quotes,colon,comments and variables

--- hello-world.js
+++ hello-world.js
@@ -1,2 +1,8 @@
 console.log("Hello World Learning JS")
 console.log(10*10)
+// Single line comments//
+/* Multi Line comments*/
+
+
+// JS you can use single quotes instead of double quoquotesates, they are the same thing
+// Semi-colon is not needed
---
+++ variables.js
@@ -0,0 +1,19 @@
+// varibale is a placeholder that you can store any value
+// var is the construct
+// Then give it a name
+// Then give it a value
+var firstName = "Marian";
+var age = 21;
+var isFemale = true;
+var balace = 100.21;
+var dob = new Date (1997, 1, 02)
+// object
+var person = {}
+var empty = undefined
+console.log(firstName);
+console.log(age);
+console.log(isFemale);
+console.log(balace)
+console.log(dob)
+console.log(person);
+console.log(empty);
