9a282583ba2daabf844cb277a1ac240c285fd39c
Abdiqani Afrah <abdi@Abdiqanis-MacBook-Air.local>
2022-08-25 12:07:36 +0100

    naming varibales

--- variables.js
+++ variables.js
@@ -6,14 +6,22 @@ var firstName = "Marian";
 var age = 21;
 var isFemale = true;
 var balace = 100.21;
+// object
 var dob = new Date (1997, 1, 02)
 // object
 var person = {}
 var empty = undefined
-console.log(firstName);
-console.log(age);
-console.log(isFemale);
-console.log(balace)
-console.log(dob)
-console.log(person);
-console.log(empty);
+console.log(typeof firstName);
+console.log(typeof age);
+console.log(typeof isFemale);
+console.log(typeof balace)
+console.log(typeof dob)
+console.log(typeof person);
+console.log(typeof empty);
+/* Varibale names
+cannot contain spaces
+must begin with a letter or an underscore(_) or dollar sign($)
+names can only contain letters, numbers, undersocres or dollar signs
+names are case sensitive
+cannot use reseved keywords
+*/
