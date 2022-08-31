2b4d22ba26b9cdb0c7b97155ad48758fcead6c1f
Abdiqani Afrah <abdi@Abdiqanis-MacBook-Air.local>
2022-08-31 13:59:54 +0100

    strings

---
+++ strings.js
@@ -0,0 +1,16 @@
+// Strings is a data type that allows you to store a sequence of characters 
+var firstName = "Abdiqani"
+console.log(firstName)
+console.log(firstName.length)
+// toUpperCase is a fucntion so you must inovke it with parentathesis toUpperCase()
+console.log(firstName.toUpperCase())
+// substring is a method
+console.log(firstName.substring(0, 4))
+
+
+// concatenate strings 
+var a = "Abdiqani"
+var b = "The Great"
+console.log(a + " " + b)
+// joining strings 
+console.log(`${a}${b.toUpperCase()}`)
